2b61b52e158d7facfccd45abb29a0f8a6c00bb0b
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-06-21 02:05:24 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class BirdsController < ApplicationController
 
   # GET /birds
@@ -15,5 +17,11 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
 
 end
